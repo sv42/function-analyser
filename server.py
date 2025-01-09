4efeba567493d5be2c7be4bc91f72a9be2f4b2a3
Vova Stoliarchuk <vovkastoliarchuk@gmail.com>
2025-01-09 19:38:55 +0000

    Run on port from env

--- server.py
+++ server.py
@@ -161,6 +161,10 @@ def index():
 
 if __name__ == '__main__':
     if '--prod' in sys.argv:
-        app.run(host='0.0.0.0', debug=False)
+        app.run(
+            host='0.0.0.0',
+            port=int(os.environ.get('PORT', 5000)),
+            debug=False
+        )
     else:
         app.run(debug=True)
